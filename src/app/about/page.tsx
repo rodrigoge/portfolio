aa168b93cb8fe4af88e49e523209f25caaa16bfc
Rodrigo Gouveia <rgestevao@gmail.com>
2025-01-11 18:14:35 -0300

    fix: fixing alt on images

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,12 +28,12 @@ export default function About() {
 
             <div className={styles.about_right_side}>
               <div className={styles.left_grid_image}>
-                <Image src={gridImage01} alt="Hero Image" loading="eager" priority/>
-                <Image src={gridImage02} alt="Hero Image" loading="eager" priority/>
+                <Image src={gridImage01} alt="About Me Image 1" loading="eager" priority/>
+                <Image src={gridImage02} alt="About Me Image 2" loading="eager" priority/>
               </div>
               <div className={styles.right_grid_image}>
-                <Image src={gridImage03} alt="Hero Image" loading="eager" priority/>
-                <Image src={gridImage04} alt="Hero Image" loading="eager" priority/>
+                <Image src={gridImage03} alt="About Me Image 3" loading="eager" priority/>
+                <Image src={gridImage04} alt="About Me Image 4" loading="eager" priority/>
               </div>
             </div>
           </div>
