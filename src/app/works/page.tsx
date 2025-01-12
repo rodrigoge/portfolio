6d6a915b7a5cdb56e282b6fdac2b9bb3d468edf6
Rodrigo Gouveia <rgestevao@gmail.com>
2025-01-11 21:10:46 -0300

    feat: implementing works page

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,11 +1,114 @@
+'use client'
+
 import Header from "@/components/Header/Header";
-import InProgress from "@/components/InProgress/InProgress";
+import { useLanguage } from "@/hooks/useLanguage";
+import Image from "next/image";
+import Link from "next/link";
+import workImg01 from '../../assets/Work01.webp';
+import workImg02 from '../../assets/Work02.webp';
+import workImg03 from '../../assets/Work03.webp';
+import workImg04 from '../../assets/Work04.webp';
+import workImg05 from '../../assets/Work05.webp';
+import workImg06 from '../../assets/Work06.webp';
+import workImg07 from '../../assets/Work07.webp';
+import workImg08 from '../../assets/Work08.webp';
+import workImg09 from '../../assets/Work09.webp';
+import workImg10 from '../../assets/Work10.webp';
+import styles from './styles.module.scss';
 
 export default function Works() {
+    const { t } = useLanguage()
     return (
         <div>
             <Header />
-            <InProgress />
+            <div className={styles.works_container}>
+                <span className={styles.title}>
+                    {t("works")}
+                </span>
+                <div className={styles.works_resume}>
+                    <span>{t("worksResume")}</span>
+                </div>
+                <div className={styles.images_container}>
+                    <Link 
+                        href={'https://www.figma.com/design/Rodw85X9fBNt7lQgHeKea1/Social?m=auto&t=dn8eLG128as8pB8h-1'} 
+                        target="_blank" 
+                        className={styles.link}
+                    >
+                        <Image src={workImg01} alt="Work Image 1" loading="eager" priority/>
+                    </Link> 
+                    <Link 
+                        href={'https://www.figma.com/design/HFHywpTt0v1tv6MjNfAqog/ReFood?m=auto&t=dn8eLG128as8pB8h-1'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg02} alt="Work Image 2" loading="eager" priority/>
+                    </Link>
+                </div>
+                <div className={styles.images_container}>
+                    <Link 
+                        href={'https://github.com/comanda-app/comanda-web'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg03} alt="Work Image 3" loading="eager" priority/>
+                    </Link>
+                    <Link 
+                        href={'https://github.com/rodrigoge/eco'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg04} alt="Work Image 4" loading="eager" priority/>
+                    </Link>
+                </div>
+                <div className={styles.images_container}>
+                    <Link 
+                        href={'https://github.com/rodrigoge/devradar'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg05} alt="Work Image 5" loading="eager" priority/>
+                    </Link>
+                    <Link 
+                        href={'https://github.com/rodrigoge/let-me-ask'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg06} alt="Work Image 6" loading="eager" priority/>
+                    </Link>
+                </div>
+                <div className={styles.images_container}>
+                    <Link 
+                        href={'https://github.com/rodrigoge/noturnal'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg07} alt="Work Image 7" loading="eager" priority/>
+                    </Link>
+                    <Link 
+                        href={'https://github.com/rodrigoge/weather-app'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg08} alt="Work Image 8" loading="eager" priority/>
+                    </Link>
+                </div>
+                <div className={styles.images_container}>
+                    <Link 
+                        href={'https://github.com/rodrigoge/globi.app'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg09} alt="Work Image 9" loading="eager" priority/>
+                    </Link>
+                    <Link 
+                        href={'https://github.com/rodrigoge/patas-felizes'} 
+                        target="_blank"
+                        className={styles.link}
+                    >
+                        <Image src={workImg10} alt="Work Image 10" loading="eager" priority/>
+                    </Link>
+                </div>
+            </div>
         </div>
     )
 }
--- src/translations/translations.ts
+++ src/translations/translations.ts
@@ -11,12 +11,13 @@ export const translations = {
         homeContent: "Working as a Software Developer for over 4 years with a focus on web applications, I have expertise and knowledge in various technologies, including Java, Spring, and React, as well as experience with other technologies and frameworks. I am passionate about innovation and creativity, with a strong focus on user experience and usability in applications.",
         homeSecondContent: "I am involved in creating digital applications that serve thousands of users in the main banks in Latin America, covering more than 19 countries. My passion for developing solutions, solving problems and intuitive design led me to the world of technology. I firmly believe that through technological innovation, we can create creative and effective solutions that improve all aspects of life, from everyday tasks to complex events.",
         inProgress: "In Progress",
-        aboutMeResume: "With experience and expertise in developing web applications and solutions, I have been part of multidisciplinary teams serving millions of users, always focusing on performance and security. Trained as a Systems Analyst, I work with a wide range of programming languages and frameworks in the market, striving to build robust and innovative products."
+        aboutMeResume: "With experience and expertise in developing web applications and solutions, I have been part of multidisciplinary teams serving millions of users, always focusing on performance and security. Trained as a Systems Analyst, I work with a wide range of programming languages and frameworks in the market, striving to build robust and innovative products.",
+        worksResume: "Here are some of the works I have done, feel free to explore and learn more about the projects.",
     },
     BR: {
         aboutMe: "Sobre Mim",
         career: "Carreira",
-        works: "Trabalhos",
+        works: "Projetos",
         contact: "Contato",
         homeWelcomeTitle: "Olá, Eu sou",
         homeNameTitle: "Rodrigo Gouveia",
@@ -25,6 +26,7 @@ export const translations = {
         homeContent: "Atuando como Desenvolvedor de Software há mais de 4 anos com foco em aplicações web, possuo expertise e conhecimento em diversas tecnologias, incluindo Java, Spring e React, além de experiência com outras tecnologias e frameworks. Sou apaixonado por inovação e criatividade, com grande foco em experiência do usuário e usabilidade em aplicações.",
         homeSecondContent: "Estou envolvido na criação de aplicações digitais que atendem a milhares de usuários nos principais bancos da América Latina, abrangendo mais de 19 países. Minha paixão pelo desenvolvimento de soluções, resolução de problemas e design intuitivo me levou ao mundo da tecnologia. Acredito firmemente que, por meio da inovação tecnológica, podemos criar soluções criativas e eficazes que melhoram todos os aspectos da vida, desde tarefas cotidianas até eventos complexos.",
         inProgress: "Em Desenvolvimento",
-        aboutMeResume: "Com experiência e expertise no desenvolvimento de aplicações e soluções web, tenho feito parte de equipes multidisciplinares atendendo milhões de usuários, sempre com foco em performance e segurança. Formado como Analista de Sistemas, trabalho com as mais diversas linguagens de programação e frameworks existentes no mercado, buscando construir produtos robustos e inovadores."
+        aboutMeResume: "Com experiência e expertise no desenvolvimento de aplicações e soluções web, tenho feito parte de equipes multidisciplinares atendendo milhões de usuários, sempre com foco em performance e segurança. Formado como Analista de Sistemas, trabalho com as mais diversas linguagens de programação e frameworks existentes no mercado, buscando construir produtos robustos e inovadores.",
+        worksResume: "Aqui estão alguns dos trabalhos que realizei, fique à vontade para explorar e aprender mais sobre os projetos.",
     },
 }
